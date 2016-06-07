b76bee64cb11b30dac8a7f212388e8f5f924f187
Konrad Rudolph <konrad.rudolph@gmail.com>
2016-06-07 19:19:24 +0100

    Add function composition

--- __init__.r
+++ __init__.r
@@ -18,6 +18,33 @@
 closure = function (formals, body, env)
     eval(call('function', as.pairlist(formals), body), env)
 
+#' Compose functions \code{g} and \code{f}.
+#'
+#' @param g a function taking as its argument the return value from \code{f}
+#' @param f a function with arbitrary arguments
+#' @return A fuction which takes the same arguments as \code{f} and returns the
+#' same return type as \code{g}
+#'
+#' @note Functions are applied in the inverse order of
+#' \code{\link{functional::Compose}}:
+#' \url{http://tolstoy.newcastle.edu.au/R/e9/help/10/02/4529.html}
+#'
+#' @note The semantics of \code{compose} are given by the equivalence
+#' \code{compose(g, f)(...) = g(f(...))}.
+#'
+#' @note All three forms of this function (\code{compose}, \code{\%.\%} and
+#' \code{\%|>\%}) are exactly identical. The only difference is the order of the
+#' arguments, which is reversed in \code{\%|>\%}. \code{\%|>\%} is thus the
+#' higher-order function counterpart to \code{\link{\%>\%}}.
+compose = function (g, f)
+    function (...) g(f(...))
+
+#' @rdname compose
+`%.%` = compose
+
+#' @rdname compose
+`%|>%` = function (f, g) compose(g, f)
+
 #' Pipe operator like in F#, Bash …
 #' @seealso \code{\link{magrittr::\%>\%}}
 `%>%` = magrittr::`%>%`
