de3b45a516900ce1e3af2486b3ddd5fa8cae65c6
Konrad Rudolph <konrad.rudolph@gmail.com>
2016-06-10 09:40:38 +0100

    Improve test names

--- _tests/test-partial.r
+++ _tests/test-partial.r
@@ -1,10 +1,10 @@
-test_that('partial positional arguments work', {
+test_that('partial works with positional arguments', {
     expect_that(p(rnorm, 5), has_formals(n = , sd = 1))
     expect_that(p(rnorm, 1, 2), has_formals(n = ))
     expect_that(p(rnorm, 1, 2, 3), has_formals())
 })
 
-test_that('partial named arguments work', {
+test_that('partial works with named arguments', {
     expect_that(p(rnorm), has_formals(n = , mean = 0, sd = 1))
     expect_that(p(rnorm, mean = 5), has_formals(n = , sd = 1))
     expect_that(p(rnorm, sd = 2), has_formals(n = , mean = 0))
