5f752538a57cd402b67bccc21d210a25adc374c1
Konrad Rudolph <konrad.rudolph@gmail.com>
2016-07-06 18:34:40 +0100

    Upgrade to testthat-1.0

--- _tests/helper-partial.r
+++ _tests/helper-partial.r
@@ -9,18 +9,18 @@ has_formals = function (...) {
 
     expected = as.pairlist(as.list(sys.call())[-1])
     function (actual)
-        expectation(identical(formals(actual), expected),
-                    sprintf('formals mismatch (expected (%s), got (%s))',
-                            f_to_s(expected), f_to_s(formals(actual))),
-                    'formals match')
+        expect(identical(formals(actual), expected),
+               sprintf('formals mismatch (expected (%s), got (%s))',
+                       f_to_s(expected), f_to_s(formals(actual))),
+               'formals match')
 }
 
 prints = function (expected) {
     function (actual) {
         printed = capture.output(actual)
-        expectation(identical(printed, expected),
-                    sprintf('does not print %s (got %s)',
-                            dQuote(expected), dQuote(printed)),
-                    'printed correctly')
+        expect(identical(printed, expected),
+               sprintf('does not print %s (got %s)',
+                       dQuote(expected), dQuote(printed)),
+               'printed correctly')
     }
 }
